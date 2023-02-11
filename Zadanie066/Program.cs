76c3d0181688d4b1bc0c2dc8a84b6e86b3a161e4
KMikhail <fin2fix@gmail.com>
2023-02-11 15:27:18 +0000

    Add Zadanie066

---
+++ Zadanie066/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+
+
+int GetNaturalNumber(string message)
+{
+  int result = 0;
+
+  while (true)
+  {
+    Console.Write(message);
+
+    if (int.TryParse(Console.ReadLine(), out result) && result > 0) break;
+    else Console.WriteLine("Ввели не число или некорректное число. Повторите ввод!");
+  }
+  return result;
+}
+
+int CountSumOfNumbers(int maxNumber, int minNumber)
+{
+  if (maxNumber == minNumber) return minNumber;
+  else return maxNumber + CountSumOfNumbers(maxNumber - 1, minNumber);
+}
+
+Console.WriteLine();
+int minNumber = GetNaturalNumber("Введите минимальное натуральное число: ");
+int maxNumber = GetNaturalNumber("Введите максимальное натуральное число: ");
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел от {minNumber} до {maxNumber} = {CountSumOfNumbers(maxNumber, minNumber)}");
